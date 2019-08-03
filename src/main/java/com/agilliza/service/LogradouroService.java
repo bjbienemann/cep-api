01f47148fa0a07b2e4f54f6b3154fb982d683c94
Brian Bienemann <brian@agilliza.com>
2019-08-02 22:09:58 -0300

    adicionado a busca por nome de rua

--- src/main/java/com/agilliza/service/LogradouroService.java
+++ src/main/java/com/agilliza/service/LogradouroService.java
@@ -10,12 +10,16 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @ApplicationScoped
 public class LogradouroService {
 
     @Inject
-    EntityManager entityManager;
+    private EntityManager entityManager;
 
     public Logradouro findByCep(String cep) {
         CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
@@ -27,5 +31,41 @@ public class LogradouroService {
 
         TypedQuery<Logradouro> typedQuery = this.entityManager.createQuery(criteriaQuery);
         return typedQuery.getSingleResult();
+    }
+
+    public List<Logradouro> findByNome(String nome) {
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<Logradouro> criteriaQuery = builder.createQuery(Logradouro.class);
+        Root<Logradouro> root = criteriaQuery.from(Logradouro.class);
+
+        Predicate predicate = builder.like(
+                builder.upper(root.get("nome")), "%"+nome.toUpperCase()+"%");
+
+        criteriaQuery.where(predicate);
+
+        TypedQuery<Logradouro> typedQuery = this.entityManager.createQuery(criteriaQuery);
+        return typedQuery.getResultList();
+    }
+
+    public Logradouro find(Map<String, Object> map) {
+        if (map.isEmpty()) {
+            return null;
+        } else {
+            CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+            CriteriaQuery<Logradouro> criteriaQuery = builder.createQuery(Logradouro.class);
+            Root<Logradouro> root = criteriaQuery.from(Logradouro.class);
+
+            List<Predicate> predicates = new ArrayList<>();
+            map.forEach((key, value) -> {
+                Predicate predicate = builder.equal(root.get(key), value);
+                predicates.add(predicate);
+            });
+
+            Predicate[] a = new Predicate[predicates.size()];
+            criteriaQuery.where(predicates.toArray(a));
+
+            TypedQuery<Logradouro> typedQuery = this.entityManager.createQuery(criteriaQuery);
+            return typedQuery.getSingleResult();
+        }
     }
 }
